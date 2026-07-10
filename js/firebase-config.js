4555c0d11f652e5d634a048ead5da354aeac92a2
alisamoo1998-oss <alisamoo1998@gmail.com>
2026-07-10 02:50:59 +0100

    Update firebase-config.js
    Signed-off-by: alisamoo1998-oss <alisamoo1998@gmail.com>

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,22 +1,19 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getFirestore, collection, addDoc, onSnapshot, doc, updateDoc, deleteDoc, query, orderBy } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBbj9zJK7QXwetU4wfM4DbZkurFSy_9v08",
+    apiKey: "...",
     authDomain: "samoo-app.firebaseapp.com",
-    databaseURL: "https://samoo-app-default-rtdb.firebaseio.com",
     projectId: "samoo-app",
     storageBucket: "samoo-app.appspot.com",
-    messagingSenderId: "474368360223",
-    appId: "1:474368360223:web:eb28dace95f8e42e02b2a2"
+    messagingSenderId: "...",
+    appId: "..."
 };
 
 const app = initializeApp(firebaseConfig);
 
 // توليد المتغيرات بالطريقتين لضمان عمل الملفات القديمة مهما كان المتغير الذي تستدعيه
 const db = getFirestore(app);
-const database = getDatabase(app);
 
 // تصدير المتغيرات للملفات الأخرى
-export { db, database, collection, addDoc, onSnapshot, doc, updateDoc, deleteDoc, query, orderBy };
+export { db, collection, addDoc, onSnapshot, doc, updateDoc, deleteDoc, query, orderBy };
