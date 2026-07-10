4fa3032fe60fa2ad3a576c5a2253c7ca2e8f2286
alisamoo1998-oss <alisamoo1998@gmail.com>
2026-07-10 12:00:09 +0100

    Export app variable along with Firestore functions
    Signed-off-by: alisamoo1998-oss <alisamoo1998@gmail.com>

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -16,4 +16,4 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 // تصدير المتغيرات للملفات الأخرى
-export { db, collection, addDoc, onSnapshot, doc, updateDoc, deleteDoc, query, orderBy };
+export { app, db, collection, addDoc, onSnapshot, doc, updateDoc, deleteDoc, query, orderBy };
