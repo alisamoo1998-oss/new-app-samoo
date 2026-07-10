fdc794aa44ac5fecc1b4a05302299c23d929a953
alisamoo1998-oss <alisamoo1998@gmail.com>
2026-07-10 01:38:13 +0100

    Add Realtime Database support in firebase config
    Updated firebase configuration to include Realtime Database and modified storage bucket URL.
    
    Signed-off-by: alisamoo1998-oss <alisamoo1998@gmail.com>

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,14 +1,19 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getFirestore, collection, addDoc, onSnapshot, doc, updateDoc, deleteDoc, query, orderBy } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js"; // استيراد الـ Realtime Database
+
 const firebaseConfig = {
-        apiKey: "AIzaSyBbj9zJK7QXwetU4wfM4DbZkurFSy_9v08",
-        authDomain: "samoo-app.firebaseapp.com",
-        projectId: "samoo-app",
-        storageBucket: "samoo-app.firebasestorage.app",
-        messagingSenderId: "474368360223",
-        appId: "1:474368360223:web:eb28dace95f8e42e02ba2a"
-    };
+    apiKey: "AIzaSyBbj9zJK7QXwetU4wfM4DbZkurFSy_9v08",
+    authDomain: "samoo-app.firebaseapp.com",
+    databaseURL: "https://samoo-app-default-rtdb.firebaseio.com", 
+    projectId: "samoo-app",
+    storageBucket: "samoo-app.appspot.com",
+    messagingSenderId: "474368360223",
+    appId: "1:474368360223:web:eb28dace95f8e42e02b2a2"
+};
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-export { db, collection, addDoc, onSnapshot, doc, updateDoc, deleteDoc, query, orderBy };
+const database = getDatabase(app); // تعريف قاعدة البيانات الفورية
+
+export { db, database, collection, addDoc, onSnapshot, doc, updateDoc, deleteDoc, query, orderBy };
