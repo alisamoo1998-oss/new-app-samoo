8c15074bf2ba64e5dd4aa9e5433ed557fabc7f61
alisamoo1998-oss <alisamoo1998@gmail.com>
2026-08-25 18:52:58 +0100

    Commit directly to the capacitor-samoo branch
    Initialize Live Update and handle its readiness state.
    
    Signed-off-by: alisamoo1998-oss <alisamoo1998@gmail.com>

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+import { LiveUpdate } from "@capawesome/capacitor-live-update";
+
 import "./firebase-config.js";
 import "./dashboard.js";
 import "./ui.js";
@@ -9,3 +11,12 @@ import "./leaveArchive.js";
 import "./leaveMissions.js";
 import "./leaveMaintenance.js";
 import "./leaveDuty.js";
+
+// إخبار نظام Live Update أن التطبيق يعمل بشكل طبيعي
+LiveUpdate.ready()
+    .then((result) => {
+        console.log("Samoo Live Update ready:", result);
+    })
+    .catch((error) => {
+        console.error("Samoo Live Update ready error:", error);
+    });
